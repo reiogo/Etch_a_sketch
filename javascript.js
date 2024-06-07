12049df9ab04e2828f4904b75c1ae6cb74e19166
reiogo <raogoda2@gmail.com>
2024-06-07 12:48:41 +0900

    Add feature: change color onmouseover event

--- javascript.js
+++ javascript.js
@@ -4,9 +4,27 @@ for (let i=0; i<16; i++) {
     gridDiv.setAttribute("id", i);
     let container = 
         document.querySelector("#container");
-    gridDiv.style = "width: 24.805vw; aspect-ratio: 1; background-color: red";
+    gridDiv.style = 
+        "width: 25%; aspect-ratio: 1; background-color: red";
     gridDiv.textContent = `hello I'm ${i}`;
     container.appendChild(gridDiv);
     
 }
+
+// Change color of divs on hover.
+let container = 
+    document.querySelector("#container");
+container.addEventListener("mouseover", (event)=> {
+        let target = event.target;
+        console.log(target);
+        // Uses randomnum generator
+        // to make random hex.
+        let randomColor =
+            '#'+Math.floor(Math.random()*
+                16777215).toString(16);
+
+        target.style.backgroundColor = randomColor;
+    })
+
+
 
