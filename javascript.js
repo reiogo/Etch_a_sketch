b8ec5d62035fdcc5e650567f2d4ccd5993b16f42
reiogo <raogoda2@gmail.com>
2024-06-07 12:07:17 +0900

    Add more js for making the grid work

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ for (let i=0; i<16; i++) {
     gridDiv.setAttribute("id", i);
     let container = 
         document.querySelector("#container");
+    gridDiv.style = "width: 25vw; height: 300px; background-color: red";
+    gridDiv.textContent = `hello I'm ${i}`;
     container.appendChild(gridDiv);
     
 }
