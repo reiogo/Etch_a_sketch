f9fe35c741a1b715ac9c6354cfb147fbeafc8952
reiogo <raogoda2@gmail.com>
2024-06-07 13:58:31 +0900

    Add functionality of changing grid size
    Had to change the code a little fit the function for removing and
    creating a new grid.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,23 @@
 // Create 16x16 grid.
-for (let i=0; i<16; i++) {
+createGrid(4);
+
+// Function for creating grids.
+function createGrid (rowNumber) {
+for (let i=0; i<(rowNumber * rowNumber); i++) {
     gridDiv = document.createElement("div");
-    gridDiv.setAttribute("id", i);
+    gridDiv.setAttribute("id", "gridDiv");
     let container = 
         document.querySelector("#container");
+    gridPercentage = 100/rowNumber;
     gridDiv.style = 
-        "width: 25%; aspect-ratio: 1; background-color: red";
-    gridDiv.textContent = `hello I'm ${i}`;
+        `width: ${gridPercentage}%; aspect-ratio: 1`;
     container.appendChild(gridDiv);
     
 }
+}
 
 // Change color of divs on hover.
-let container = 
+const container = 
     document.querySelector("#container");
 container.addEventListener("mouseover", (event)=> {
         let target = event.target;
@@ -26,5 +31,29 @@ container.addEventListener("mouseover", (event)=> {
         target.style.backgroundColor = randomColor;
     })
 
+
+// Button for changing grid size
+const btn = document.createElement("button");
+    btn.style.fontSize ="18px";
+btn.textContent =
+    "Choose a grid size less then 100";
+document.body.insertBefore(btn, container);
+
+btn.addEventListener("click", ()=>{
+    userAnswer = prompt(
+        "Enter a grid size less than 100");
+    container.replaceChildren();
+
+    while (userAnswer >= 100) {
+        userAnswer = prompt(
+            "That wasn't valid, try again: Enter a value less then 100" )
+    }
+    if (userAnswer < 100) {
+    createGrid(userAnswer);
+    }
+
+    
+    
+});
 
 
